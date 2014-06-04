d02319664afded541172c0429b9cdf75a74fbc7b
akila <akila.aki28@gmail.com>
2014-06-04 15:50:13 +0530

    updated

--- app/controllers/deposits_controller.rb
+++ app/controllers/deposits_controller.rb
@@ -20,6 +20,13 @@ end
 
 def index
   @deposits = Deposit.all
+  @deposits = Deposit.paginate(page: params[:page], per_page: 10)
 end
 
+def destroy
+  @deposit = Deposit.find(params[:id])
+  @deposit.destroy
+  flash[:notice] = "customer has been deleted"
+  redirect_to deposits_path
+end
 end
